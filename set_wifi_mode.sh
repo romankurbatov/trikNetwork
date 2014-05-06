3119a93056e789faf912d807d39aba7d1457aa45
Roman Kurbatov <roman.kurbatov.1992@gmail.com>
2014-05-06 14:33:19 +0400

    fixed a bug with generating hostapd config

--- set_wifi_mode.sh
+++ set_wifi_mode.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+hostapd_conf=/etc/hostapd.conf
 trikrc=/etc/trik/trikrc
 interface=wlan0
 
